2aadfbe0cfd9fbd9b7d5930d08cc2c4ce69805a3
LucioD93 <luciodavid93@gmail.com>
2017-03-01 17:27:57 -0400

    Eliminados archivos innecesarios

--- Dept/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class departamento(models.Model):
-    nombre = models.CharField(max_length = 100)
-    area = models.CharField(max_length = 100)
-    
-    def __str__(self):
-        return self.nombre
