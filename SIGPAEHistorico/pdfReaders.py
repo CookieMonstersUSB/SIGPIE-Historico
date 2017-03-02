923375bfb4e736d2085ed59620648766a8e9f1f8
LucioD93 <luciodavid93@gmail.com>
2017-03-01 21:14:01 -0400

    Sustituidos metodos de lectura por pdfminer

--- SIGPAEHistorico/pdfReaders.py
+++ SIGPAEHistorico/pdfReaders.py
@@ -10,48 +10,75 @@ import pyocr.builders
 import io
 import PyPDF2
 
+from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO, BytesIO
+
 PDF_LANG = 'eng'
 
-def LeerPDFimagenes(archivo) :
+# def LeerPDFimagenes(archivo) :
+#
+#     imagenes = []
+#     texto_final = []
+#
+#     jpg = Image(file = archivo)
+#
+#     for i in jpg.sequence :
+#         img_pag = Image(image=i)
+#         imagenes.append(img_pag. make_blob('jpeg'))
+#
+#     for i in imagenes :
+#         txt = tool.image_to_string (
+#             PI.open(io.BytesIO(i)),
+#             lang= PDF_LANG,
+#             builder = pyocr.builders.TextBuilder()
+#             )
+#         texto_final.append(txt)
+#
+#     return '\n'.join(texto_final)
+def LeerPDFimagenes(archivo):
+    scrape = archivo
+    pdfFiler = BytesIO(scrape.read())
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
 
-    #lenguaje = tool.get_available_languages()[0]
-    print(tool.get_available_languages())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(pdfFiler, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
 
-    imagenes = []
-    texto_final = []
-
-    #pdf = Image(file=archivo, resolution=300)
-    #jpg = pdf.convert('jpeg')
-    #stream = archivo.open()
-    jpg = Image(file = archivo)
-    
-
-    for i in jpg.sequence :
-        print("aca")
-        img_pag = Image(image=i)
-        imagenes.append(img_pag. make_blob('jpeg'))
-    
-    for i in imagenes :
-        print("aqui")
-        txt = tool.image_to_string (
-            PI.open(io.BytesIO(i)),
-            lang= PDF_LANG,
-            builder = pyocr.builders.TextBuilder()
-            )
-    
-        
-        texto_final.append(txt)
-        
-
-    return '\n'.join(texto_final)
+    device.close()
+    textstr = retstr.getvalue()
+    retstr.close()
+    return textstr
 
 def leerPDFtexto(stream):
-    print(stream)
-    pdfReader = PyPDF2.PdfFileReader(stream= stream)
-    k = pdfReader.numPages
-    text = ''
-    for i in range (k):
-        pagina = pdfReader.getPage(i)
-        text += pagina.extractText()
-    
-    return text
+    scrape = stream
+    pdfFiler = BytesIO(scrape.read())
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(pdfFiler, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    device.close()
+    textstr = retstr.getvalue()
+    retstr.close()
+    return textstr
