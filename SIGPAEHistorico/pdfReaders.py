4fc82b8120dce8fccd1cd53ab09831f3257d0ed4
LucioD93 <luciodavid93@gmail.com>
2017-03-02 12:51:05 -0400

    PDFMiner integrado. Vista HTML

--- SIGPAEHistorico/pdfReaders.py
+++ SIGPAEHistorico/pdfReaders.py
@@ -3,19 +3,20 @@ Created on Feb 16, 2017
 
 @author: Sergio
 '''
-from wand.image import Image
-from PIL import Image as PI
-from pyocr import tesseract as tool
-import pyocr.builders
-import io
-import PyPDF2
+# from wand.image import Image
+# from PIL import Image as PI
+# from pyocr import tesseract as tool
+# import pyocr.builders
+# import io
+# import PyPDF2
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
+from pdfminer.converter import TextConverter, HTMLConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO, BytesIO
+import io
 
 PDF_LANG = 'eng'
 
@@ -39,7 +40,7 @@ PDF_LANG = 'eng'
 #         texto_final.append(txt)
 #
 #     return '\n'.join(texto_final)
-def LeerPDFimagenes(archivo):
+def LeerPDFaString(archivo):
     scrape = archivo
     pdfFiler = BytesIO(scrape.read())
     rsrcmgr = PDFResourceManager()
@@ -61,24 +62,21 @@ def LeerPDFimagenes(archivo):
     retstr.close()
     return textstr
 
-def leerPDFtexto(stream):
-    scrape = stream
-    pdfFiler = BytesIO(scrape.read())
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
+def leerPDFaHTML(stream):
+    pagenums = set()
+    manager = PDFResourceManager()
     codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
     caching = True
-    pagenos=set()
-    for page in PDFPage.get_pages(pdfFiler, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+    output = io.BytesIO()
+    converter = HTMLConverter(manager, output, codec=codec, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+    infile = open("output.html", 'w')
+    for page in PDFPage.get_pages(stream, pagenums,caching=caching, check_extractable=True):
         interpreter.process_page(page)
 
-    device.close()
-    textstr = retstr.getvalue()
-    retstr.close()
-    return textstr
+    convertedPDF = output.getvalue()
+
+    infile.close()
+    converter.close()
+    output.close()
+    return convertedPDF
--- SIGPAEHistorico/urls.py
+++ SIGPAEHistorico/urls.py
@@ -7,7 +7,6 @@ from .views import editar
 
 urlpatterns = [
         url(r'^$', index.as_view(), name='index'),
-        url(r'^editor/$', hola.as_view(), name='Hola Mundo'),
         url(r'^upload/$', upload.as_view(), name ='upload'),
         url(r'^listar/$', listar.as_view(), name ='listar'),
         url(r'^editar/$', editar.as_view(), name ='editar')
