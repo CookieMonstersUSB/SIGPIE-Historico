5a1f81ee5b408b527fefc8149925ff70fd233419
LucioD93 <luciodavid93@gmail.com>
2017-03-05 19:13:16 -0400

    Migracion a postgresql y agregado al readme

--- ProyectoIngenieria/settings.py
+++ ProyectoIngenieria/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ProyectoIngenieria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sigpae_hist_db',
+        'USER': 'cmusb',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
