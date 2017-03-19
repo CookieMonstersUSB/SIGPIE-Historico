5b5f6abcd91650545bcfa1ec79274ba50b918ef0
Sergio Medina <Sergio Medina>
2017-03-19 00:42:28 -0400

    sql files uploaded

---
+++ static/scriptsbd/dependenciasUSB.sql
@@ -0,0 +1,71 @@
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (0, '--------', 0);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (1, 'FÃ­sica', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (2, 'QuÃ­mica', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (3, 'MecÃ¡nica', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (4, 'MatemÃ¡ticas Puras y Aplicadas', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (5, 'Computo CientÃ­fico y EstadÃ­stica', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (6, 'ElectrÃ³nica y Circuitos', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (7, 'TermodinÃ¡mica y FenÃ³menos de Transferencia', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (8, 'ConversiÃ³n y Transporte de EnergÃ­a', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (9, 'Procesos y Sistemas', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (10, 'Ciencias de los Materiales', 1);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (11, 'Ciencias de la Tierra', 1);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (12, 'Ciencia y TecnologÃ­a del Comportamiento', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (13, 'Lengua y Literatura', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (14, 'Ciencias EconÃ³micas y Administrativas', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (15, 'Idiomas', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (16, 'FilosofÃ­a', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (17, 'Ciencias Sociales', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (18, 'Arquitectura y Artes PlÃ¡sticas', 2);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (19, 'PlanificaciÃ³n Urbana', 2);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (20, 'BiologÃ­a Celular', 3);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (21, 'Estudios Ambientales', 3);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (22, 'BiologÃ­a de Organismos', 3);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (23, 'TecnologÃ­a de Procesos BiolÃ³gicos y BioquÃ­micos', 3);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (24, 'TecnologÃ­a de Servicios', 4);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (25, 'TecnologÃ­a Industrial', 4);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (26, 'FormaciÃ³n General y Ciencias BÃ¡sicas', 4);
+
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (27, 'Ciclo BÃ¡sico', 5);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (28, 'Ciclo Profesional', 5);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (29, 'FormaciÃ³n General', 5);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (30, 'Ciclo de IniciaciÃ³n Universitaria', 5);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (31, 'FÃ­sica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (32, 'QuÃ­mica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (33, 'BiologÃ­a', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (34, 'Ingenieria MecÃ¡nica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (35, 'MatemÃ¡tica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (36, 'TecnologÃ­a e IngenierÃ­a ElÃ©ctrica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (37, 'TecnologÃ­a e IngenierÃ­a ElectrÃ³nica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (38, 'Ingeniera QuÃ­mica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (39, 'Ingeniera de ComputaciÃ³n', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (41, 'IngenierÃ­a GeofÃ­sica', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (42, 'IngenierÃ­a de Materiales', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (43, 'IngenierÃ­a de ProducciÃ³n y OrganizaciÃ³n Empresarial', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (44, 'TecnologÃ­a MecÃ¡nica, Mantenimiento AeronÃ¡utico e Ingeniera de Mantenimiento', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (45, 'IngenierÃ­a de telecomunicaciones', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (46, 'Arquitectura', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (47, 'Estudios Generales', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (48, 'Turismo, HotelerÃ­a y Hospitalidad', 6);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (49, 'Comercio Exterior y Licenciatura en Comercio Internacional', 6);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (50, 'TecnologÃ­a e IngenierÃ­a ElÃ©ctrica', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (51, 'TecnologÃ­a ElÃ©ctrica y ElectrÃ³nica', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (52, 'IngenierÃ­a de ProducciÃ³n y OrganizaciÃ³n Empresarial', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (53, 'Turismo, HotelerÃ­a y Hospitalidad', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (54, 'AdministraciÃ³n Aduanera', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (55, 'Comercio Exterior y Licenciatura en Comercio Internacional', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (56, 'AdministraciÃ³n del Transporte y OrganizaciÃ³n Empresarial', 7);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (57, 'TecnologÃ­a MecÃ¡nica, Mantenimiento AeronÃ¡utico e IngenierÃ­a de Mantenimiento', 7);
+
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (58, 'Ciencias BÃ¡sicas y Aplicadas', 8);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (59, 'Ciencias Sociales y Humanidades', 8);
+INSERT INTO "SIGPAEHistorico_dependencias" VALUES (60, 'IngenierÃ­a y TecnologÃ­a', 8);
+
+--INSERT INTO "SIGPAEHistorico_dependencias" VALUES (, '', );
---
+++ static/scriptsbd/divisionesUSB.sql
@@ -0,0 +1,10 @@
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (0, '--------');
+
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (1, 'División Ciencias Físicas y Matemáticas');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (2, 'División Ciencias Sociales y Humanidades');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (3, 'División Ciencias Biológicas');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (4, 'División Ciencias y Tecnologías Administrativas e Industriales');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (5, 'Estudios Generales');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (6, 'Estudios Profesionales');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (7, 'Estudios Tecnológicos');
+INSERT INTO "SIGPAEHistorico_divisiones" VALUES (8, 'Estudios de Postgrado');
