f9852638b25be17270bdbf1c48878f68ef23eb72
LucioD93 <luciodavid93@gmail.com>
2017-03-28 14:46:03 -0400

    Terminada historia 9

--- SIGPAEHistorico/urls.py
+++ SIGPAEHistorico/urls.py
@@ -3,10 +3,11 @@ from .views import *
 
 
 urlpatterns = [
-        url(r'^$', index.as_view(), name='index'),
-        url(r'^upload/$', upload.as_view(), name ='upload'),
-        url(r'^listar/$', listar.as_view(), name ='listar'),
-        url(r'^editar/(?P<pkdoc>\d+)/$', editar.as_view(), name ='editar'),
-        url(r'^consultarpae/$', consultarpae.as_view(), name ='consultarpae'),
-        url(r'^mostrarpae/(?P<code>\w+)/(?P<year>\d+)$', mostrarpae.as_view(), name ='mostrarpae')
+		url(r'^$', index.as_view(), name='index'),
+		url(r'^upload/$', upload.as_view(), name ='upload'),
+		url(r'^listar/$', listar.as_view(), name ='listar'),
+		url(r'^editar/(?P<pkdoc>\d+)/$', editar.as_view(), name ='editar'),
+		url(r'^consultarpae/$', consultarpae.as_view(), name ='consultarpae'),
+		url(r'^mostrarpae/(?P<code>\w+)/(?P<year>\d+)/$', mostrarpae.as_view(), name ='mostrarpae'),
+		url(r'^mostrarpae/(?P<code>\w+)/$', mostrarpae.as_view(), name ='mostrarpae')
     ]
